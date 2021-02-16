bafa250fcda37988c0c6e300f87315089646040a
Phumai <mcc3939@gmail.com>
2021-02-16 16:43:34 +0700

    fix

--- lib/pages/signin/components/body.dart
+++ lib/pages/signin/components/body.dart
@@ -4,10 +4,47 @@ import 'package:flutter_svg/svg.dart';
 import '../../../constants.dart';
 import '../../../size_config.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({
     Key key,
   }) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  FocusNode _emailFocus = new FocusNode();
+  FocusNode _passwordFocus = new FocusNode();
+
+  double _animatedContainerHeight = 190;
+  double _animatedContainerWidth = 190;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailFocus.addListener(_onFocusChange);
+    _passwordFocus.addListener(_onFocusChange);
+  }
+
+  void _onFocusChange() {
+    setState(() {
+      if (_emailFocus.hasFocus || _passwordFocus.hasFocus) {
+        _animatedContainerHeight = 90;
+        _animatedContainerWidth = 90;
+      } else {
+        _animatedContainerHeight = 190;
+        _animatedContainerWidth = 190;
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _emailFocus.dispose();
+    _passwordFocus.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,127 +53,128 @@ class Body extends StatelessWidget {
       height: double.infinity,
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 30),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 30),
-              Center(
-                child: Image.asset(
-                  "assets/images/image1.png",
-                  width: 190,
-                  height: 190,
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 30),
+            AnimatedContainer(
+              duration: kAnimationDuration,
+              child: Image.asset(
+                "assets/images/image1.png",
+                width: _animatedContainerWidth,
+                height: _animatedContainerHeight,
               ),
-              SizedBox(height: 30),
-              Text(
-                "Login to you account",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
-                ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              "Login to you account",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
               ),
-              SizedBox(height: 30),
-              TextField(
-                style: TextStyle(
-                  color: Colors.black,
+            ),
+            SizedBox(height: 30),
+            TextField(
+              focusNode: _emailFocus,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22,
+              ),
+              decoration: InputDecoration(
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                hintText: "name@domain.com",
+                hintStyle: TextStyle(
+                  color: Colors.black12,
                   fontSize: 22,
                 ),
-                decoration: InputDecoration(
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                  hintText: "name@domain.com",
-                  hintStyle: TextStyle(
-                    color: Colors.black12,
-                    fontSize: 22,
-                  ),
-                  labelText: "Email",
-                  labelStyle: TextStyle(fontSize: 20),
-                ),
+                labelText: "Email",
+                labelStyle: TextStyle(fontSize: 20),
               ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  SizedBox(
-                    width: SizeConfig.screenWidth * 0.6,
-                    child: TextField(
-                      obscureText: true,
-                      style: TextStyle(
-                        color: Colors.black,
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                SizedBox(
+                  width: SizeConfig.screenWidth * 0.6,
+                  child: TextField(
+                    focusNode: _passwordFocus,
+                    obscureText: true,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                    ),
+                    decoration: InputDecoration(
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      hintText: "************",
+                      hintStyle: TextStyle(
+                        color: Colors.black12,
                         fontSize: 22,
                       ),
-                      decoration: InputDecoration(
-                        floatingLabelBehavior: FloatingLabelBehavior.always,
-                        hintText: "************",
-                        hintStyle: TextStyle(
-                          color: Colors.black12,
-                          fontSize: 22,
-                        ),
-                        labelText: "Password",
-                        labelStyle: TextStyle(fontSize: 20),
-                      ),
+                      labelText: "Password",
+                      labelStyle: TextStyle(fontSize: 20),
                     ),
                   ),
-                  Spacer(),
-                  Container(
-                    width: 55,
-                    height: 55,
-                    margin: EdgeInsets.only(top: 15),
-                    decoration: BoxDecoration(
-                      color: kInactiveColor,
-                      borderRadius: BorderRadius.circular(16),
-                    ),
-                    child: Icon(
-                      Icons.chevron_right,
-                      size: 35,
-                      color: Colors.white,
+                ),
+                Spacer(),
+                Container(
+                  width: 55,
+                  height: 55,
+                  margin: EdgeInsets.only(top: 15),
+                  decoration: BoxDecoration(
+                    color: kInactiveColor,
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Icon(
+                    Icons.chevron_right,
+                    size: 35,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40),
+            Center(child: Text("or", style: TextStyle(fontSize: 18))),
+            SizedBox(height: 40),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: kFacebookColor,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(
+                    width: 20,
+                    child: SvgPicture.asset(
+                      "assets/icons/facebook.svg",
+                      color: kFacebookColor,
                     ),
                   ),
+                  Text(
+                    "Login With Facebook",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: kFacebookColor,
+                    ),
+                  ),
+                  SizedBox(width: 20),
                 ],
               ),
-              SizedBox(height: 40),
-              Center(child: Text("or", style: TextStyle(fontSize: 18))),
-              SizedBox(height: 40),
-              Container(
-                height: 50,
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(12),
-                  border: Border.all(
-                    color: kFacebookColor,
-                  ),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: 20,
-                      child: SvgPicture.asset(
-                        "assets/icons/facebook.svg",
-                        color: kFacebookColor,
-                      ),
-                    ),
-                    Text(
-                      "Login With Facebook",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                        color: kFacebookColor,
-                      ),
-                    ),
-                    SizedBox(width: 20),
-                  ],
-                ),
+            ),
+            SizedBox(height: 50),
+            Center(
+              child: Text(
+                "Forgot Password?",
+                style: TextStyle(fontSize: 16),
               ),
-              SizedBox(height: 50),
-              Center(
-                child: Text(
-                  "Forgot Password?",
-                  style: TextStyle(fontSize: 16),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
